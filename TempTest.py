1984df9abdc9f31df605c73faa3d42875cc8c08d
xautshuanglong <xjshuanglong@126.com>
2016-05-13 01:29:06 +0800

    clear Fibo.py Fibo.pyc TempTest.py

--- TempTest.py
+++ TempTest.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # encoding:utf-8
 
 '''
@@ -13,12 +12,3 @@ print fibo(2000)
 from Fibo import fibonacci
 
 print fibonacci(200)
-=======
-print '<======================================>'
-
-import sys
-from Fibo import fibonacci
-fibonacci(10)
-
-print '<================= end ================>'
->>>>>>> 957e5dd92fc24c050425f05f3dcbca7d30dea087
