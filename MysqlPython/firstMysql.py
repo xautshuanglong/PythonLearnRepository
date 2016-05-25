d1fda7de78e3e6c617761af49a64e693559cc9e5
xautshuanglong <xjshuanglong@126.com>
2016-05-25 19:30:29 +0800

    mysql test

--- MysqlPython/firstMysql.py
+++ MysqlPython/firstMysql.py
@@ -1,3 +1,14 @@
 # encoding:utf-8
 
 import MySQLdb
+
+db = MySQLdb.connect('10.1.1.159','shuanglong','shuanglong','test')
+cursor = db.cursor()
+cursor.execute("select version()")
+data = cursor.fetchone()
+cursor.execute("select * from t_test")
+res = cursor.fetchall()
+for record in res:
+	print record
+print 'Database Version: %s' % data
+db.close()
