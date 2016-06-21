9d7a2cfba3ffd38a709745de7e658c2f8ac78ca8
xautshuanglong <xjshuanglong@126.com>
2016-06-21 11:51:48 +0800

    Http 优化

--- HttpLearn/HttpTest.py
+++ HttpLearn/HttpTest.py
@@ -7,18 +7,16 @@ log = LogTool.getLogger()
 
 httpConn = None
 try:
-    httpConn = httplib.HTTPConnection("www.sina.com.cn")
-    log.debug("entry try block...")
-    httpConn.request('get','/')
-    log.debug("after request...")
+    httpConn = httplib.HTTPConnection("192.168.1.110", 9090)
+    httpConn.request('GET','/index.html')
     response = httpConn.getresponse()
-    log.debug("after getresponse")
     print response.status
     print response.reason
     print response.getheaders()
+    log.debug(response.read())
     log.debug("end of try block...")
 except Exception,err:
-    print err
+    print "Error:",err
 except:
     print "unknow error occurred"
 finally:
