f0daa8562a5bdb22ca7031e526b08ca17b74ca0c
Shuanglong <xjshuanglong@126.com>
2022-08-24 00:14:16 +0800

    print 升级到 3.0+ 格式

--- TempTest.py
+++ TempTest.py
@@ -82,38 +82,38 @@ for item in lists:
 table = {'key1':111,'key2':222,'key3':333}
 print('key1:{key1:d},key2:{key2:#d},key3:{key3:#x}'.format(**table))
 # 位置参数
-print "{0} is {1} years old".format("Wilber", 28)
-print "{} is {} years old".format("Wilber", 28)
-print "Hi, {1}! {1} is {0} years old".format(28,"Wilber")
+print("{0} is {1} years old".format("Wilber", 28))
+print("{} is {} years old".format("Wilber", 28))
+print("Hi, {1}! {1} is {0} years old".format(28,"Wilber"))
 
 # 关键字参数
-print "{name} is {age} years old".format(name = "Wilber", age = 28)
+print("{name} is {age} years old".format(name = "Wilber", age = 28))
 
 # 下标参数
 li = ["Wilber", 28]
-print "{0[0]} is {0[1]} years old".format(li)
+print("{0[0]} is {0[1]} years old".format(li))
 
 # 填充与对齐
 # ^、<、>分别是居中、左对齐、右对齐，后面带宽度
 # :号后面带填充的字符，只能是一个字符，不指定的话默认是用空格填充
-print '{:>8}'.format('3.14')
-print '{:<8}'.format('3.14')
-print '{:^8}'.format('3.14')
-print '{:0>8}'.format('3.14')
-print '{:a>8}'.format('3.14')
+print('{:>8}'.format('3.14'))
+print('{:<8}'.format('3.14'))
+print('{:^8}'.format('3.14'))
+print('{:0>8}'.format('3.14'))
+print('{:a>8}'.format('3.14'))
 
 # 浮点数精度
-print '{:.4f}'.format(3.1415926)
-print '{:0>10.4f}'.format(3.1415926)
+print('{:.4f}'.format(3.1415926))
+print('{:0>10.4f}'.format(3.1415926))
 
 # 进制
 # b、d、o、x分别是二进制、十进制、八进制、十六进制
-print '{:b}'.format(11)
-print '{:d}'.format(11)
-print '{:o}'.format(11)
-print '{:x}'.format(11)
-print '{:#x}'.format(11)
-print '{:#X}'.format(11)
+print('{:b}'.format(11))
+print('{:d}'.format(11))
+print('{:o}'.format(11))
+print('{:x}'.format(11))
+print('{:#x}'.format(11))
+print('{:#X}'.format(11))
 
 # 千位分隔符
-print '{:,}'.format(1570000000)
+print('{:,}'.format(1570000000))
--- TimeTest.py
+++ TimeTest.py
@@ -1,3 +1,11 @@
+'''
+Author: Shuanglong xjshuanglong@126.com
+Date: 2022-08-23 23:08:56
+LastEditors: Shuanglong xjshuanglong@126.com
+LastEditTime: 2022-08-23 23:13:37
+FilePath: \PythonLearnRepository\TimeTest.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 # coding:utf-8
 
 import sys,time,locale
@@ -11,5 +19,5 @@ printf(locale.getpreferredencoding)
 
 timeStr = unicode('当前时间：','utf-8')
 while(1):
-	print timeStr+time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+	print(timeStr+time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
 	time.sleep(1)
